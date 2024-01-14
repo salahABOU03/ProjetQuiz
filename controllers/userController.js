ed78eb5d28c6400bd693b9e4b1d008ec43d72c20
Salama Anas <salamawork404@gmail.com>
2024-01-14 02:14:33 +0100

    fixing login

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,9 +10,10 @@ exports.registerUser = async (req, res) => {
       return res.status(400).json({ error: 'Username already exists' });
     }
 
-    const hashedPass = await bcrypt.hash(password,10)
+    const hashedPass = await bcrypt.hash(password,10);
 
     const user = await new User({ fullName, username, password:hashedPass, role });
+    await user.save();
     res.status(201).json({ message: 'User created', user });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -23,17 +24,23 @@ exports.loginUser = async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    const isMatch = await bcrypt.compare(password,user.password);
-    if (!user || !isMatch) {
+
+    if (!user) {
       return res.status(401).json({ message: 'Invalid username or password' });
     }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    
     const userData = {
       _id: user._id,
       fullName: user.fullName,
       username: user.username,
       role: user.role,
     };
-    res.status(200).json({user: userData});
+
+    res.status(200).json({ user: userData });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
